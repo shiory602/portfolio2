adb899c5e288785c70c5cb5a28b0b503be36869b
shiory602 <suzushioufo@gmail.com>
2024-02-07 12:19:53 +0400

    change data

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-$(document).ready(function(){
-	
-ScrollReveal().reveal('.reveal', { 
-	delay: 100,
-  duration: 1700,
-  origin: "bottom",
-  distance: "100px",
-  opacity: 0,
-	reset: true,
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".gnav").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
 });
 
-ScrollReveal().reveal('.revealslower', { 
-	delay: 700,
-  duration: 1700,
-  origin: "bottom",
-  distance: "100px",
-  opacity: 0,
-	reset: true,
+////////////////////
+/* ハンバーガーmenu */
+////////////////////
+// 追従ナビ・スマホでハンバーガーメニューになる
+$(".nav_toggle").on("click", function () {
+  $(".nav_toggle, .nav").toggleClass("show");
 });
-
-})
-
+$(".nav_menu_li").on("click", function () {
+  $(".nav_toggle, .nav").toggleClass("show");
+});
